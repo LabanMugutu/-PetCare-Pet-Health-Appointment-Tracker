4a625eeb8493fb1d3374d9d4f1bc78c101dd4eae
LabanMugutu <labanmugutu@gmail.com>
2025-10-16 15:18:59 +0300

    Begins working on Owner class

--- lib/models/owner.py
+++ lib/models/owner.py
@@ -6,3 +6,6 @@ This class behaves as a simple ORM mapping rows <-> Python objects.
 from typing import Optional, List
 import sqlite3
 from lib.database import CURSOR, CONN
+
+class Owner:
+    def __init__(self, name: str, contact: str, id: Optional[int] = None):
